284884346919ffbaeaa86d196f6ecc052bb4230c
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-03-30 20:14:58 -0300

    feat: add post

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 import express from "express";
 
+const port = process.env.PORT ?? "8080"
 const mock = [
     {id: 1, name: "John"},
     {id: 2, name: "Doe"}
 ]
 
 const app = express()
-const port = process.env.PORT ?? "8080"
+app.use(express.json())
 
 app.get("/users", (req, res) => {
     res.type('json');
     res.send(mock)
+})
+
+app.post("/users", async (req, res) => {
+    const user = req.body
+    mock.push(user)
+    res.send(`id: ${user?.id}`)
 })
 
 console.log(`API running on port ${port}`)
