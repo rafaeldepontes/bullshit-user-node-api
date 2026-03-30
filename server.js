5dd12668939ffc5f43569f49180488af43c754ec
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-03-30 20:27:59 -0300

    feat: add find by id

--- server.js
+++ server.js
@@ -10,14 +10,19 @@ const app = express()
 app.use(express.json())
 
 app.get("/users", (req, res) => {
-    res.type('json');
-    res.send(mock)
+    res.status(200).json(mock)
+})
+
+app.get("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = mock.find((u) => u.id === id)
+    user ? res.status(200).json(user) : res.status(404).json({message: "User not found"})
 })
 
 app.post("/users", async (req, res) => {
     const user = req.body
     mock.push(user)
-    res.send(`id: ${user?.id}`)
+    res.status(201).json({id: user?.id})
 })
 
 console.log(`API running on port ${port}`)
